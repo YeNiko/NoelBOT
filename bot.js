1f8d0623f05964ea95e42f832ab270dabdb1d824
YeNiko <38641774+YeNiko@users.noreply.github.com>
2018-06-18 19:08:46 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -60,6 +60,12 @@ bot.on("message", async message => {
         message.channel.send("https://www.twitch.tv/overwatchleague");
     }
 
+    if(command === `${prefix}add`) {
+        let numArray = args.map(n=> parseInt(n));
+        let total = numArray.reduce( (p, c) => p+c);
+        
+        message.channel.sendMessage(total));
+    }
     
     if(command === `${prefix}roll`) {
         var roll = Math.floor(Math.random() * 6) + 1;
